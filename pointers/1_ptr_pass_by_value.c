4bff44b9ae69a1a9dc9f10613c1b58cbf3c4f1df
Atul Raut <rauji.raut@gmail.com>
2021-10-23 08:48:26 -0700

    cptr: Rearrange code base
    Sorting as per understanding
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

---
+++ pointers/1_ptr_pass_by_value.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
+
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+#define mysizeof(arr) (char*)&arr+1 - (char*)&arr
+#define arrsz(x) sizeof(x) / sizeof(x[0])
+
+void test_ptr() {
+        int a[10];
+        int* p = a;
+
+        assert(sizeof(a) / sizeof(a[0]) == 10);
+        assert(sizeof(p) == sizeof(int*));
+        assert(sizeof(*p) == sizeof(int));
+}
+
+void test_sizeof() {
+        int arr[] = {1,2,3,4};
+        printf ("\nsz-->%ld \n", mysizeof(arr));
+        printf ("\nsz-->%ld \n", arrsz(arr));
+}
+
+int a = 20;
+void myfun(int* p) {
+    p = &a;
+}
+int main() {
+    int x = 10;
+    int* p = &x;
+    myfun(p);
+    printf("x = %d\n", x);
+
+    test_sizeof();
+    test_ptr();
+    return 0;
+}
